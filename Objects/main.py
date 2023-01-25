348a6a3c5abebcc7168555ebb61ca7108deb7ec7
Emerson Okorie <emersonokorie@icloud.com>
2023-01-25 20:21:52 -0300

    new

---
+++ Objects/main.py
@@ -0,0 +1,34 @@
+import turtle
+jerry = turtle.Turtle()
+print(jerry)
+
+jerry.shape("turtle")
+jerry.color("blue")
+jerry.pencolor("brown")
+jerry.forward(100)
+jerry.left(90)
+jerry.forward(100)
+jerry.left(90)
+jerry.forward(100)
+jerry.left(90)
+jerry.forward(100)
+
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.pencolor("blue")
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+
+
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
