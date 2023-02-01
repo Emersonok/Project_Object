0dcf400a2a2e610cf7c6f9c2b470558e1a14376b
Emerson Okorie <emersonokorie@icloud.com>
2023-02-01 19:25:54 -0300

    update

--- turtle_race.py
+++ turtle_race.py
@@ -1,69 +1,28 @@
 from turtle import Turtle, Screen
 import random
-steps = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-jerry_steps = 0
-timmy_steps = 0
-tom_steps = 0
-robbo_steps = 0
-game_over = False
 screen = Screen()
+screen.setup(500, 400)
+choice = screen.textinput("Select", "Choose a winner")
+is_game_on = False
+all_turtles = []
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.penup() 
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-90, -50, -10, 30, 70, 110]
 
-jerry = Turtle()
-jerry.shape("turtle")
-jerry.color("blue")
-jerry.penup()
+for turtle_index in range(0, 6):
+   new_turtle = Turtle("turtle")
+   new_turtle.color(colors[turtle_index])
+   new_turtle.penup() 
+   new_turtle.goto(-230, y_positions[turtle_index])
 
-tom = Turtle()
-tom.shape("turtle")
-tom.color("green")
-tom.penup()
+if choice:
+    is_game_on = True
 
-robbo = Turtle()
-robbo.shape("turtle")
-robbo.penup()
-
-def start_position():
-    timmy.setheading(340)
-    timmy.backward(370)
-    timmy.setheading(0)
-    jerry.setheading(350)
-    jerry.backward(355)
-    jerry.setheading(0)
-    tom.backward(350)  
-    robbo.setheading(380)
-    robbo.backward(370)
-    robbo.setheading(0)
-
-def move():
-    timmy_step = timmy.forward(random.choice(steps))
-    timmy_step += timmy_steps
-    jerry_step = jerry.forward(random.choice(steps))
-    jerry_step += jerry_steps
-    tom_step = tom.forward(random.choice(steps))
-    tom_step += tom_steps
-    robbo_step = robbo.forward(random.choice(steps))
-    robbo_step += robbo_steps
-
-
-
-
-screen.listen()
-screen.onkey(start_position, "space")
-
-while not game_over:
-  screen.onkey(move, "Return")
-
-  if timmy[steps]== 500 or jerry[steps] == 500 or tom[steps] ==500 or robbo[steps]==500:
-    game_over = True
-
-
-
-
+while is_game_on:
+    for turtle in all_turtles:
+        rand_distance =random.randint(0, 10)
+        turtle.forward(rand_distance)
+    
 
 screen = Screen()
 screen.exitonclick()
