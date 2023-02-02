0ea197336989e2266aff2209a6fa2acce4ec100a
Emerson Okorie <emersonokorie@icloud.com>
2023-02-02 10:49:36 -0300

    update

--- turtle_race.py
+++ turtle_race.py
@@ -9,28 +9,41 @@ all_turtles = []
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-90, -50, -10, 30, 70, 110]
 
-for turtle_index in range(0, 6):
-   new_turtle = Turtle("turtle")
-   new_turtle.color(colors[turtle_index])
-   new_turtle.penup() 
-   new_turtle.goto(-230, y_positions[turtle_index])
-   all_turtles.append(new_turtle)
+#use 'for' to multiply the turtles by 6
+#create the turtle called new_turtle
+#give each turtle a color from colors variable
+#make the turtles penup
+#create the initial position of turtles using goto(x,y)
+#add all the turles to all_turtles list
+for each_turtle in range(0, 6):
+    new_turtle = Turtle("turtle")
+    new_turtle.color(colors[each_turtle])
+    new_turtle.penup()
+    new_turtle.goto(-230, y_positions[each_turtle])
+    all_turtles.append(new_turtle)
 
+
+
+#start the game after user selects
 if user_choice:
     is_game_on = True
-
+#use while to start the game
+#loop through the all turtles list 
+#if the x.cor() surpasses 230, then the game ends
+#create a variable to get color of the winning turtle
+#messages for if the player wins or not
+#get the random distances at which turtles move
+#ask the turtles to move
 while is_game_on:
     for turtle in all_turtles:
         if turtle.xcor() > 230:
             is_game_on = False
-            winning_color = turtle.pencolor()
-            if winning_color ==  user_choice:
-                print(f"You've won! The {winning_color} turtle is the winner")
+            winning_turtle = turtle.pencolor()
+            if user_choice == winning_turtle:
+                print(f"You win! The {winning_turtle} is the winner")
             else:
-                print(f"You've lost! The {winning_color} turtle is the winner")
-        rand_distance =random.randint(0, 10)
-        turtle.forward(rand_distance)
-    
-
+                print(f"You lose! The {winning_turtle} is the winner")
+        random_pace = random.randint(0, 6)
+        turtle.forward(random_pace)
 screen = Screen()
 screen.exitonclick()
